a0ed59ca15263efab18a758b518e8773c9ded4c6
Filo44 <98215610+Filo44@users.noreply.github.com>
2022-04-14 16:53:41 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,40 @@
+console.log("ready")
+
+var menu=document.querySelector(".menu")
+var first=document.querySelector(".first")
+var second=document.querySelector(".second")
+var third=document.querySelector(".third")
+
+menu.addEventListener("click",()=>{
+    menu.style.animation="slidingmenu 1s forwards"
+    first.style.animation="first 1s forwards"
+    second.style.animation="second 1s forwards"
+    third.style.animation="third 1s forwards"
+    setTimeout(()=>{
+        menu.innerHTML=`
+       
+        <a href="#" class="menu link sidelist">menu</a>
+        <br>
+        <a href="index.html" class="link sidelist">Home</a>
+        <br>
+        <a href="code.html" class="link sidelist">Code</a>
+        <br>
+        <a href=mailto:filocle4444@gmail.com class="link sidelist">Contact</a>
+        `
+        document.querySelector("body").addEventListener("click",()=>{
+            menu.innerHTML=`<a href="#" class="menu link">menu</a>`
+            first.style.animation="firstreverse 1s forwards"
+            second.style.animation="secondreverse 1s forwards"
+            third.style.animation="thirdreverse 1s forwards"
+            
+            console.log(menu)
+            menu.style="animation:slidingmenureverse 1s forward;"
+            setTimeout(()=>{
+                
+            })
+    },500)
+
+    
+    })
+})
+
